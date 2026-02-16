70e8d880fe833d66913e2c614bae0f393b2da886
Zurard <zurard07@gmail.com>
2026-02-16 10:03:20 +0530

    animation changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            transitionDuration: {
+                '400': '400ms',
+                '600': '600ms',
+            },
+            trasnsitionTimingFunction: {
+               'smooth': 'cubic-bezier(0.25, 0.46, 0.45, 0.94)',    
+            'bounce': 'cubic-bezier(0.34, 1.56, 0.64, 1)',
+            },
+        },
     },
     plugins: [],
 }
